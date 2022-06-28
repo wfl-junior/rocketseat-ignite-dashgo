5674b38035feef1dee88914a87de9a34299b0528
Wallace Júnior <wflj1997@hotmail.com>
2022-06-28 20:42:40 -0300

    Configurando MirageJS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,12 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import { Fragment } from "react";
 import { SidebarDrawerContextProvider } from "../contexts/SidebarDrawerContext";
+import { makeServer } from "../services/mirage";
 import { theme } from "../styles/theme";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => (
   <Fragment>
---
+++ src/services/mirage/index.ts
@@ -0,0 +1,25 @@
+import { createServer, Model } from "miragejs";
+
+interface User {
+  name: string;
+  email: string;
+  created_at: string;
+}
+
+export function makeServer() {
+  return createServer({
+    models: {
+      user: Model.extend<Partial<User>>({}),
+    },
+    routes() {
+      this.namespace = "api";
+      this.timing = 750;
+
+      this.get("/users");
+      this.post("/users");
+
+      this.namespace = "";
+      this.passthrough();
+    },
+  });
+}
