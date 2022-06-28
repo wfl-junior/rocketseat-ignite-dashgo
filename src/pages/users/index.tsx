68c189e49e742a91f9e0ec486436dd11db463cbe
Wallace Júnior <wflj1997@hotmail.com>
2022-06-28 11:46:51 -0300

    Componente: Pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
 import { NextPage } from "next";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 const Users: NextPage = () => (
@@ -91,6 +92,8 @@ const Users: NextPage = () => (
             ))}
           </Tbody>
         </Table>
+
+        <Pagination />
       </Box>
     </Flex>
   </Box>
