ac4a6f24786bbd0961ffa141a392528d76bb6947
Wallace Júnior <wflj1997@hotmail.com>
2022-06-28 00:35:38 -0300

    Configurando Chakra UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { NextPage } from "next";
 import { AppProps } from "next/app";
+import { theme } from "../styles/theme";
 
-const App: NextPage<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+const App: NextPage<AppProps> = ({ Component, pageProps }) => (
+  <ChakraProvider theme={theme}>
+    <Component {...pageProps} />
+  </ChakraProvider>
+);
 
 export default App;
