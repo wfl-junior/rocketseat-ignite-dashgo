d0e9d929abb5e2921101160d0c94c61ba799d83e
Wallace Júnior <wflj1997@hotmail.com>
2022-06-28 02:01:59 -0300

    Página: Dashboard

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -22,14 +22,16 @@ export const Sidebar: React.FC = () => (
         <Stack spacing={4} mt={8} align="stretch">
           <Link display="flex" alignItems="center">
             <Icon as={RiDashboardLine} fontSize={20} />
-            <Text ml={4} fontWeight="medium">
+
+            <Text as="span" ml={4} fontWeight="medium">
               Dashboard
             </Text>
           </Link>
 
           <Link display="flex" alignItems="center">
             <Icon as={RiContactsLine} fontSize={20} />
-            <Text ml={4} fontWeight="medium">
+
+            <Text as="span" ml={4} fontWeight="medium">
               Usuários
             </Text>
           </Link>
@@ -49,14 +51,16 @@ export const Sidebar: React.FC = () => (
         <Stack spacing={4} mt={8} align="stretch">
           <Link display="flex" alignItems="center">
             <Icon as={RiInputMethodLine} fontSize={20} />
-            <Text ml={4} fontWeight="medium">
+
+            <Text as="span" ml={4} fontWeight="medium">
               Formulários
             </Text>
           </Link>
 
           <Link display="flex" alignItems="center">
             <Icon as={RiGitMergeLine} fontSize={20} />
-            <Text ml={4} fontWeight="medium">
+
+            <Text as="span" ml={4} fontWeight="medium">
               Automação
             </Text>
           </Link>
