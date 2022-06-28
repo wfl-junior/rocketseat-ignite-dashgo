7d4f08c957d49600bce964da528eba83c1cda135
Wallace Júnior <wflj1997@hotmail.com>
2022-06-28 01:42:34 -0300

    Componente: Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,20 @@
+import { Flex } from "@chakra-ui/react";
 import { NextPage } from "next";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 interface DashboardProps {}
 
 const Dashboard: NextPage<DashboardProps> = () => {
-  return <Header />;
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my={6} maxW={1480} mx="auto" px={6}>
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Dashboard;
