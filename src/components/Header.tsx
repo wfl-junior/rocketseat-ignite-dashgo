fd4f8cb8d707b7649ada61fb14c01ff9f5066bfd
Wallace Júnior <wflj1997@hotmail.com>
2022-06-28 01:32:00 -0300

    Finalizando Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
-import { Flex, Icon, Input, Text } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri";
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from "@chakra-ui/react";
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from "react-icons/ri";
 
 export const Header: React.FC = () => (
   <Flex
@@ -37,12 +41,42 @@ export const Header: React.FC = () => (
         color="gray.50"
         variant="unstyled"
         placeholder="Buscar na plataforma"
-        px={4}
+        // px={4}
         mr={4}
         _placeholder={{ color: "gray.400" }}
       />
 
       <Icon as={RiSearchLine} fontSize={20} />
+    </Flex>
+
+    <Flex align="center" ml="auto">
+      <HStack
+        spacing={8}
+        mx={8}
+        pr={8}
+        py={1}
+        color="gray.300"
+        borderRightWidth={1}
+        borderColor="gray.700"
+      >
+        <Icon as={RiNotificationLine} fontSize={20} />
+        <Icon as={RiUserAddLine} fontSize={20} />
+      </HStack>
+
+      <Flex align="center">
+        <Box mr={4} textAlign="right">
+          <Text>Wallace Júnior</Text>
+          <Text color="gray.300" fontSize="small">
+            wflj1997@gmail.com
+          </Text>
+        </Box>
+
+        <Avatar
+          size="md"
+          name="Wallace Júnior"
+          src="https://github.com/wfl-junior.png"
+        />
+      </Flex>
     </Flex>
   </Flex>
 );
